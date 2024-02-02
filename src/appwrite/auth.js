3b5c2c65355320c9a23b320137c83e0058af829a
surajpathak39 <surajpathak39@gmail.com>
2024-02-02 08:51:55 +0530

    feature: megha project

---
+++ src/appwrite/auth.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-useless-catch */
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+// const client = new Client()
+//     .setEndpoint() // Your API Endpoint
+//     .setProject('<PROJECT_ID>');               // Your project ID
+
+// const account = new Account(client);
+
+// const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+// promise.then(function (response) {
+//     console.log(response); // Success
+// }, function (error) {
+//     console.log(error); // Failure
+// });
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteURL) // Your API Endpoint
+      .setProject(conf.appwriteProjectId); // Your project ID
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,5 @@
+const Container = ({ children }) => {
+  return <div className="w-full max-w-7xl mx-auto pax-4">{children}</div>;
+};
+
+export default Container;
